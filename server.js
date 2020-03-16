10b8de69cb25b6ef4b29b612927eacbfaebab30d
wbrink <will.brink55@gmail.com>
2020-03-16 12:05:08 -0700

    added option to get port from environment variable

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require("path");
 
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 
 // this middleware is used for parsing request body found in POST and PUT requests
